48bb82d2793b9fd241421ca740007039fddbefd6
Marcos Gonzalez <marcos.abraham2002@gmail.com>
2023-03-06 18:10:42 -0700

    Comprobación de los resultados con los de la tabla

--- router/index.js
+++ router/index.js
@@ -3,7 +3,25 @@ const router = express.Router();
 const bodyParse = require("body-parser");
 
 router.get('/',(req,res)=>{
-    res.render('examen.html')
+    const valores = {
+        numRecibo: req.query.numRecibo,
+        nombre: req.query.nombre,
+        domicilio: req.query.domicilio,
+        tipo: req.query.tipo,
+        kilowats: req.query.kilowats
+    }
+    res.render('examen.html',valores)
+ });
+
+ router.post('/',(req,res)=>{
+    const valores = {
+        numRecibo: req.body.numRecibo,
+        nombre: req.body.nombre,
+        domicilio: req.body.domicilio,
+        tipo: req.body.tipo,
+        kilowats: req.body.kilowats
+    }
+    res.render('resultados.html',valores)
  });
 
 module.exports=router;
